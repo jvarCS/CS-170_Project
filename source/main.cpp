333debbf4d730011e16367a8d730355981b19888
jvarCS <vargassegoviano@gmail.com>
2024-05-05 21:14:19 -0700

    Added Problem Interface

--- source/main.cpp
+++ source/main.cpp
@@ -1,132 +1,51 @@
 #include <iostream>
 #include "../header/boards.h"
+#include "../header/problem.h"
 #include <cmath>
 #include <queue>
 #include <unordered_set>
 
 using namespace std;
 
-void UCS();
-bool checkBoard(Boards);
-float euclidean(char **);
-int findPlace(char **, char, float &);
-void eucStar();
-bool solve(Boards);
-string makeString(Boards); 
-float findGPlace(char **, char, float &);
+Problem *menu(Boards **);
 
 int main() {
-    /*char *arr = new char(10);
-    arr[0] = '8';
-    arr[1] = '2';
-    arr[2] = '3';
-    arr[3] = '4';
-    arr[4] = '5';
-    arr[5] = '6';
-    arr[6] = '7';
-    arr[7] = '8';
-    arr[8] = 'x';*/
-    Boards start;
-
-    start.print();
+    Boards *start;
+    Problem *game = menu(&start);
+    
 
     //delete []arr;
     return 0;
 }
 
-bool solve(Boards start) {
-    int moves = 0;  // Total moves.
-    queue<Boards> boardQ;   // Queue of boards.
-    unordered_set<string> uset;     // hash set to see if curr board has already been explored.
-
-    boardQ.push(start);     // Pushes first board into queue.
-
-    while (true) {
-        if (boardQ.empty()) return false;   // Check if queue is empty. Return false if it is.
-        Boards temp = boardQ.front();   // Set temp boards var to the board at front of queue.
-        boardQ.pop();   // Remove board at front of queue.
-        if (checkBoard(temp)) return true;  // Checks if current board is goal board.
-        uset.insert(makeString(temp));     // Marks current board as explored.
-        // Expand current board. Here is where the diff search algos will be called.
-        // In each, their diff priority reqs will be calc as well, i.e, in eucStar, calc
-        // priority as euclidean distance + total number of moves. In UCS, just total moves.
-        
+Problem *menu(Boards **start) {
+    char choice, algo;
+    cout << "Welcome to 862242193 8 puzzle solver.\n"
+         << "Enter '1' to use Uniform Search Cost, '2' for Misplace Tile A*, '3' for Euclidean A*\n";
+    cin  >> algo;
+    cout << "Enter '1' to use default puzzle, or '2' to enter your own puzzle.\n";
+    cin  >> choice;
+    
+    // If user enters 1, we use default board size and numbering
+    if (choice == '1') {
+        Problem *ex = new Problem(algo);
+        *start = new Boards();
+        return ex;
     }
-}
-
-void UCS() {
-
-}
-
-
-void eucStar(char **arr) {
-
-}
-
-bool checkBoard(Boards board) {
-    char num = '1';
-    for (int i = 0; i < 3; i++) {       // Hardcoded size to 3 here. Change later
-        for (int j = 0; j < 3; j++) {
-            if (board.getNum(i,j) != num and board.getNum(i,j) != 'x') return false;
-            num++;
-        }
+    else if (choice == '2') {
+        int size;
+        string nums;
+        cout << "The board will be of NxN measurement. Please enter the value for N.\n";
+        cin >> size;
+        Problem *ex = new Problem(size,algo);
+        cout << "Enter the numbers in the desired order, with 'x' representing the blank space.\n"
+             << "Example: 123\n"
+             << "         456\n"
+             << "         78x\n"
+             << "for a 3x3 board would be entered as '12345678x'.\n";
+        cin >> nums;
+        *start = new Boards(nums,size);
+        return ex;
     }
-    return true;
-}
-
-// Calculates euclidean distance of current board
-float euclidean(char **arr) {
-    int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-    float sum = 0;
-    char num = '1';
-
-    for (int i = 0; i < 3; i++) {       // Hardcoded size to 3 here. Change later
-        for (int j = 0; j < 3; j++) {
-            if (arr[i][j] != num and arr[i][j] != 'x') {
-                x1 = i; y2 = j;     // x1,y1 = goal position for number
-                x2 = findPlace(arr,num,y2);     // x2,y2 = actual position on board
-                sum += sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-            }
-            num++;
-        }
-    }
-
-}
-
-// Finds actual xy coordinate in board of a given number
-int findPlace(char **arr, char target, int &idx) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (arr[i][j] == target) {
-                idx = j;    //return y2
-                return i;   //return x2
-            }
-        }
-    }
-}
-
-// Finds goal xy coordinate in board of a given number
-float findGPlace(char arr[3][3], char target, float &idx) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (arr[i][j] == target) {
-                idx = j;    //return y2
-                return i;   //return x2
-            }
-        }
-    }
-
-    return 0;
-}
-
-
-string makeString(Boards board) {
-    string numbers;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            numbers += board.getNum(i,j);
-        }
-    }
-
-    return numbers;
+    return nullptr;
 }
